c54b095c67afa2d83a9c8f717d3beb58389d2572
Viacheslav_Bondariev <rdy2qa@gmail.com>
2021-10-23 12:26:40 +0300

    write

--- test/specs/test.ts
+++ test/specs/test.ts
@@ -2,5 +2,6 @@ describe("", () => {
     it('should ', async () => {
         await browser.url("https://google.com");
         await browser.url("https://odessa.ithillel.ua/coaches/olga-chekmaeva");
+        await browser.close();
     });
 })
