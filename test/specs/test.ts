21358903fc68268cef83b54c32afd46c4bd701a6
Viacheslav_Bondariev <rdy2qa@gmail.com>
2021-10-25 15:08:00 +0300

    check webhook

--- test/specs/test.ts
+++ test/specs/test.ts
@@ -3,5 +3,6 @@ describe("TEST", () => {
         await browser.url("https://google.com");
         await browser.url("https://odessa.ithillel.ua/coaches/olga-chekmaeva");
         await browser.url("http://beta.webdriver.io/docs/allure-reporter.html");
+        await browser.url("https://odessa.ithillel.ua/coaches/olga-chekmaeva");
     });
 })
